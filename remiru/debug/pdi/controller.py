8e3190d4edb5ce4e901ecab2b417316281cec6ee
dragonmux <git@dragonmux.network>
2023-03-06 22:04:03 +0000

    debug/pdi/controller: Begun implementing instruction decoding and the read/write/repeat counter logic

--- remiru/debug/pdi/controller.py
+++ remiru/debug/pdi/controller.py
@@ -30,10 +30,13 @@ class PDIController(Elaboratable):
 
 		parity = self.dataIn[0:8].xor()
 
+		data = Signal(8)
 		opcode = Signal(PDIOpcodes)
 		args = Signal(4)
 		readCount = Signal(32)
 		writeCount = Signal(32)
+		repCount = Signal(32)
+		updateCounts = Signal()
 
 		# This FSM implements handling getting data into and out of the PDI controller
 		with m.FSM(name = 'pdiFSM'):
@@ -44,6 +47,7 @@ class PDIController(Elaboratable):
 					self.parityError.eq(0),
 					readCount.eq(0),
 					writeCount.eq(0),
+					repCount.eq(0),
 				]
 				m.next = 'IDLE'
 
@@ -63,23 +67,59 @@ class PDIController(Elaboratable):
 				m.next = 'DISPATCH-BYTE'
 
 			with m.State('DISPATCH-BYTE'):
+				# Check if we've got a parity error and dispatch on it
 				with m.If(self.parityError):
 					m.d.sync += self.busy.eq(0)
 					m.next = 'IDLE'
+				# Else if we're in an idle state, treat the new byte as an opcode
 				with m.Elif(opcode == PDIOpcodes.IDLE):
 					m.next = 'DECODE-INSN'
+				# Else if we have more data to write dispatch to HANDLE-WRITE
 				with m.Elif(writeCount != 0):
 					m.next = 'HANDLE-WRITE'
+				# Else we must have more data to read, so dispatch to HANDLE-READ
 				with m.Else():
 					m.next = 'HANDLE-READ'
 
+				# If we didn't get a parity error, copy the data into the internal data register
+				with m.If(~self.parityError):
+					m.d.sync += data.eq(self.dataIn[0:8])
+
 			with m.State('DECODE-INSN'):
-				pass
+				# Unpack the instruction into the internal instruction registers
+				m.d.sync += [
+					opcode.eq(data[5:8]),
+					args.eq(data[0:4]),
+				]
+				m.d.comb += updateCounts.eq(1)
+				m.next = 'IDLE'
 
 			with m.State('HANDLE-READ'):
 				pass
 
 			with m.State('HANDLE-WRITE'):
 				pass
+
+		sizeA = Signal(5)
+		sizeB = Signal(5)
+		repeatData = Signal(32)
+
+		m.d.comb += [
+			sizeA.eq(args[2:4] + 1),
+			sizeB.eq(args[0:2] + 1),
+		]
+
+		with m.FSM(name = 'insnFSM'):
+			with m.State('IDLE'):
+				# If the main FSM asks us to update counts, then dispatch to the instruction decoder
+				with m.If(updateCounts):
+					m.next = 'DECODE'
+
+			with m.State('DECODE'):
+				# Decode based on the opcode
+				with m.Switch(opcode):
+					with m.Case(PDIOpcodes.IDLE):
+						m.d.sync += repCount.eq(0)
+						m.next = 'IDLE'
 
 		return m
