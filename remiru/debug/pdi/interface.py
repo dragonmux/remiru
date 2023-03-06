29d0180b0ba5f85d2704ced378552aa35ad61abe
dragonmux <git@dragonmux.network>
2023-03-06 15:31:45 +0000

    debug/pdi: Built some of the CDC logic to go between the JTAG and PDI domains

--- remiru/debug/pdi/interface.py
+++ remiru/debug/pdi/interface.py
@@ -1,6 +1,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 from torii import Elaboratable, Module, Signal, Const
 from torii.build import Platform
+from torii.lib.cdc import FFSynchronizer, PulseSynchronizer
 
 __all__ = (
 	'PDIInterface',
@@ -20,28 +21,56 @@ class PDIInterface(Elaboratable):
 
 		self.pdiDataIn = Signal(8)
 		self.pdiDataOut = Signal(8)
+		self.pdiBusy = Signal()
+		self.pdiDone = Signal()
+		self.pdiDoneAck = Signal()
+		self.pdiNextReady = Signal()
 
 	def elaborate(self, platform: Platform):
 		m = Module()
 
-		# The JTAG controller PDI signals are on the JTAG clock domain and
+		nextReady = Signal()
+		useRequest = Signal()
+		awaitingResponse = Signal()
+		haveResponse = Signal()
+		responseAck = Signal()
+		#pdiResponse = Signal(9)
+		jtagResponse = Signal(9)
+
+		responseAckSync = PulseSynchronizer(i_domain = 'jtag', o_domain = 'sync')
+
+		# NB: The JTAG controller PDI signals are on the JTAG clock domain and
 		# must be FFSynchronizer'd over to the main CPU clock domain for the
 		# PDI controller, which exclusively sits on the main clock domain.
+		m.submodules += [
+			FFSynchronizer(self.pdiBusy, awaitingResponse, o_domain = 'jtag'),
+			FFSynchronizer(self.pdiDone, haveResponse, o_domain = 'jtag'),
+			responseAckSync,
+			FFSynchronizer(nextReady, self.pdiNextReady, o_domain = 'sync'),
+			FFSynchronizer(self.jtagDataIn, self.pdiDataIn, o_domain = 'sync'),
+			FFSynchronizer(self.pdiDataOut, jtagResponse, o_domain = 'jtag'),
+		]
 
-		haveResponse = Signal()
-		awaitingResponse = Signal()
-		pdiResponse = Signal(9)
+		m.d.comb += [
+			responseAckSync.i.eq(responseAck),
+			self.pdiDoneAck.eq(responseAckSync.o),
+		]
 
 		# If we have a response when the JTAG machinary asks for one, then we emit it, invalidating it.
 		# Else, if we are awaiting a response then we respond with the PDI "Delay Byte" response till we
 		# get one. Otherwise we must answer with the PDI "Empty Byte" response
 		with m.If(self.jtagNeedsResponse):
+			m.d.sync += useRequest.eq(~awaitingResponse)
 			with m.If(haveResponse):
-				m.d.comb += self.jtagDataOut.eq(pdiResponse)
-				m.d.jtag += haveResponse.eq(0)
+				m.d.comb += self.jtagDataOut.eq(jtagResponse)
+				m.d.comb += responseAck.eq(1)
 			with m.Elif(awaitingResponse):
 				m.d.comb += self.jtagDataOut.eq(PDI_DELAY_BYTE)
 			with m.Else():
 				m.d.comb += self.jtagDataOut.eq(PDI_EMPTY_BYTE)
+
+		# If awaitngResponse was low during the Capture-DR phase, then throw away the
+		# request, otherwise forward it on to the PDI controller
+		m.d.comb += nextReady.eq(useRequest & self.jtagHasRequest)
 
 		return m
