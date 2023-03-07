55fdb6557b2efcf226c1f8639964e3cf2dd09460
dragonmux <git@dragonmux.network>
2023-03-07 01:25:02 +0000

    debug/pdi/controller: Refactored the counter update FSM to try and improve encoding and speed

--- remiru/debug/pdi/controller.py
+++ remiru/debug/pdi/controller.py
@@ -134,73 +134,70 @@ class PDIController(Elaboratable):
 						m.next = 'IDLE'
 					# The next 4 cases handle the PDI debug bus load/store instructions
 					with m.Case(PDIOpcodes.LDS):
-						m.next = 'LDS'
+						# LDS instructions specify how many bytes to read in sizeA
+						# and how many bytes to write in sizeB
+						m.d.sync += [
+							writeCount.eq(sizeB),
+							readCount.eq(sizeA),
+						]
+						m.next = 'HANDLE-REPEAT'
 					with m.Case(PDIOpcodes.LD):
-						m.next = 'LD'
+						# LD instructions specify how many bytes to write in sizeB
+						# the instruction does not read any bytes
+						m.d.sync += [
+							writeCount.eq(sizeB),
+							readCount.eq(0),
+						]
+						m.next = 'HANDLE-REPEAT'
 					with m.Case(PDIOpcodes.STS):
-						m.next = 'STS'
+						# STS instructions specify how many bytes to read in sizeA + sizeB
+						# the instruction does not write any bytes
+						m.d.sync += [
+							writeCount.eq(0),
+							readCount.eq(sizeA + sizeB),
+						]
+						m.next = 'HANDLE-REPEAT'
 					with m.Case(PDIOpcodes.ST):
-						m.next = 'ST'
+						# ST instructions specify how many bytes to read in sizeB
+						# the instruction does not write any bytes
+						m.d.sync += [
+							writeCount.eq(0),
+							readCount.eq(sizeB),
+						]
+						m.next = 'HANDLE-REPEAT'
 					# The next 2 cases handle the PDI controller register load/store instructions
 					with m.Case(PDIOpcodes.LDCS):
-						m.next = 'LDCS'
+						# LDCS instructions only write a single byte and never read any bytes
+						m.d.sync += [
+							writeCount.eq(1),
+							readCount.eq(0),
+						]
+						m.next = 'HANDLE-REPEAT'
 					with m.Case(PDIOpcodes.STCS):
-						m.next = 'STCS'
+						# STCS instructions only read a single byte and never write any bytes
+						m.d.sync += [
+							writeCount.eq(0),
+							readCount.eq(1),
+						]
+						m.next = 'HANDLE-REPEAT'
 					# The repeat instruction is a special-case and must ignore the repeat counter
 					with m.Case(PDIOpcodes.REPEAT):
-						m.next = 'REPEAT'
+						# REPEAT instructions specify how many bytes to read in sizeB
+						# the instruction does not write any bytes
+						m.d.sync += [
+							writeCount.eq(0),
+							readCount.eq(sizeB),
+						]
+						m.next = 'CAPTURE-REPEAT'
 					# The key instruction is a special-case and must clear the repeat counter
 					with m.Case(PDIOpcodes.KEY):
-						m.next = 'KEY'
-
-			with m.State('LDS'):
-				# LDS instructions specify how many bytes to read in sizeA
-				# and how many bytes to write in sizeB
-				m.d.sync += [
-					writeCount.eq(sizeB),
-					readCount.eq(sizeA),
-				]
-				m.next = 'HANDLE-REPEAT'
-			with m.State('LD'):
-				# LD instructions specify how many bytes to write in sizeB
-				# the instruction does not read any bytes
-				m.d.sync += [
-					writeCount.eq(sizeB),
-					readCount.eq(0),
-				]
-				m.next = 'HANDLE-REPEAT'
-
-			with m.State('STS'):
-				# STS instructions specify how many bytes to read in sizeA + sizeB
-				# the instruction does not write any bytes
-				m.d.sync += [
-					writeCount.eq(0),
-					readCount.eq(sizeA + sizeB),
-				]
-				m.next = 'HANDLE-REPEAT'
-			with m.State('ST'):
-				# ST instructions specify how many bytes to read in sizeB
-				# the instruction does not write any bytes
-				m.d.sync += [
-					writeCount.eq(0),
-					readCount.eq(sizeB),
-				]
-				m.next = 'HANDLE-REPEAT'
-
-			with m.State('LDCS'):
-				# LDCS instructions only write a single byte and never read any bytes
-				m.d.sync += [
-					writeCount.eq(1),
-					readCount.eq(0),
-				]
-				m.next = 'HANDLE-REPEAT'
-			with m.State('STCS'):
-				# STCS instructions only read a single byte and never write any bytes
-				m.d.sync += [
-					writeCount.eq(0),
-					readCount.eq(1),
-				]
-				m.next = 'HANDLE-REPEAT'
+						# KEY instructions imply 8 bytes to read and never write any bytes
+						m.d.sync += [
+							writeCount.eq(0),
+							readCount.eq(8),
+							repCount.eq(0),
+						]
+						m.next = 'IDLE'
 
 			with m.State('HANDLE-REPEAT'):
 				# If a repeat count is in play, then we need to decrement it now the instruction
@@ -209,14 +206,6 @@ class PDIController(Elaboratable):
 					m.d.sync += repCount.eq(repCount - 1)
 				m.next = 'IDLE'
 
-			with m.State('REPEAT'):
-				# REPEAT instructions specify how many bytes to read in sizeB
-				# the instruction does not write any bytes
-				m.d.sync += [
-					writeCount.eq(0),
-					readCount.eq(sizeB),
-				]
-				m.next = 'CAPTURE-REPEAT'
 			with m.State('CAPTURE-REPEAT'):
 				# Each time the control state machine indicates it got another byte for the repeat count,
 				# shift that into the bottom of the repeatData register until we satisfy the read count
@@ -237,16 +226,6 @@ class PDIController(Elaboratable):
 					with m.Case(3):
 						m.d.sync += repCount.eq(Cat(repeatData[24:32],
 							repeatData[16:24], repeatData[8:16], repeatData[0:8]))
-				m.next = 'IDLE'
-
-			with m.State('KEY'):
-				# KEY instructions imply 8 bytes to read and never write any bytes
-				m.d.sync += [
-					writeCount.eq(0),
-					readCount.eq(8),
-					# KEY instructions must clear the repeat counter
-					repCount.eq(0),
-				]
 				m.next = 'IDLE'
 
 		return m
